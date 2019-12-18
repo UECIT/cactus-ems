17f3567512edfe217d3a8d7a535620aeb434260d
Michael Houston <michael.houston3@nhs.net>
2019-12-18 10:13:22 +0000

    Bugfix: Logging empty response body

--- src/main/java/uk/nhs/ctp/config/RequestLogging.java
+++ src/main/java/uk/nhs/ctp/config/RequestLogging.java
@@ -2,7 +2,7 @@ package uk.nhs.ctp.config;
 
 import static java.util.Arrays.asList;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,11 +15,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
 
@@ -29,7 +30,11 @@ public class RequestLogging {
 
   @Bean
   public RestTemplateCustomizer restTemplateCustomizer(Interceptor interceptor) {
-    return restTemplate -> restTemplate.getInterceptors().add(interceptor);
+    return restTemplate -> {
+      restTemplate.getInterceptors().add(interceptor);
+      restTemplate.setRequestFactory(
+          new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+    };
   }
 
   @Component
@@ -47,19 +52,13 @@ public class RequestLogging {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final ObjectMapper objectMapper;
-
-    public Interceptor(ObjectMapper objectMapper) {
-      this.objectMapper = objectMapper;
-    }
-
     @Override
     public ClientHttpResponse intercept(
         HttpRequest request, byte[] body,
         ClientHttpRequestExecution execution) throws IOException {
 
       logRequest(request, body);
-      ClientHttpResponse response = new ResponseWrapper(execution.execute(request, body));
+      ClientHttpResponse response = execution.execute(request, body);
       logResponse(request, response);
       return response;
     }
@@ -69,8 +68,10 @@ public class RequestLogging {
       log.info("URI         : {}", request.getURI());
       log.info("Method      : {}", request.getMethod());
       log.info("Headers     : {}", request.getHeaders());
-      MediaType contentType = request.getHeaders().getContentType();
-      formatBody(new ByteArrayInputStream(body), contentType);
+      if (body.length > 0) {
+        MediaType contentType = request.getHeaders().getContentType();
+        formatBody(new ByteArrayInputStream(body), contentType);
+      }
       log.info("============= request end ({}) ===============", request.hashCode());
     }
 
@@ -82,68 +83,26 @@ public class RequestLogging {
       log.info("Status text  : {}", response.getStatusText());
       log.info("Headers      : {}", response.getHeaders());
 
-      MediaType contentType = response.getHeaders().getContentType();
-      formatBody(response.getBody(), contentType);
+      String contentLengthHeader = response.getHeaders().getFirst(HttpHeaders.CONTENT_LENGTH);
+      if (response.getStatusCode().is2xxSuccessful() && (
+          Strings.isNullOrEmpty(contentLengthHeader)
+              || response.getHeaders().getContentLength() > 0)) {
+        MediaType contentType = response.getHeaders().getContentType();
+        formatBody(response.getBody(), contentType);
+      }
       log.info("=========== response end ({}) ================", request.hashCode());
     }
 
     private void formatBody(InputStream body, MediaType contentType) throws IOException {
       if (jsonContentTypes.stream().anyMatch(contentType::isCompatibleWith)) {
-        String responseBody = StreamUtils.copyToString(body, contentType.getCharset());
-        log.info("Response body: {}", responseBody);
+        String bodyString = StreamUtils.copyToString(body, contentType.getCharset());
+        log.info("Body: {}", bodyString);
       } else if (textContentTypes.stream().anyMatch(contentType::isCompatibleWith)) {
-        String responseBody = StreamUtils.copyToString(body, contentType.getCharset());
-        log.info("Response body: {}", responseBody);
+        String bodyString = StreamUtils.copyToString(body, contentType.getCharset());
+        log.info("Body: {}", bodyString);
       } else {
-        log.info("Request body: NON TEXT: {}", contentType);
+        log.info("Body: NON TEXT: {}", contentType);
       }
-    }
-
-  }
-
-  /**
-   * Based on org.springframework.http.client.BufferingClientHttpResponseWrapper
-   */
-  final static class ResponseWrapper implements ClientHttpResponse {
-
-    private final ClientHttpResponse response;
-    private byte[] body;
-
-    ResponseWrapper(ClientHttpResponse response) {
-      this.response = response;
-    }
-
-    @Override
-    public HttpStatus getStatusCode() throws IOException {
-      return this.response.getStatusCode();
-    }
-
-    @Override
-    public int getRawStatusCode() throws IOException {
-      return this.response.getRawStatusCode();
-    }
-
-    @Override
-    public String getStatusText() throws IOException {
-      return this.response.getStatusText();
-    }
-
-    @Override
-    public HttpHeaders getHeaders() {
-      return this.response.getHeaders();
-    }
-
-    @Override
-    public InputStream getBody() throws IOException {
-      if (this.body == null) {
-        this.body = StreamUtils.copyToByteArray(this.response.getBody());
-      }
-      return new ByteArrayInputStream(this.body);
-    }
-
-    @Override
-    public void close() {
-      this.response.close();
     }
 
   }
