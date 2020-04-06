812666096fa60e6175341a32ffbfce55c59761d7
Charlie Marcus <charlie.marcus1@nhs.net>
2020-04-06 16:46:57 +0100

    fix audits querying old table

--- src/main/java/uk/nhs/ctp/service/search/AuditSearchResultDTO.java
+++ src/main/java/uk/nhs/ctp/service/search/AuditSearchResultDTO.java
@@ -1,50 +1,16 @@
 package uk.nhs.ctp.service.search;
 
 import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class AuditSearchResultDTO {
 
 	private Long id;
 	private String firstName;
 	private String lastName;
-	private Date timestamp;
-	
-	public AuditSearchResultDTO(Long id, String firstName, String lastName, Date timestamp) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.timestamp = timestamp;
-	}
+	private Date createdDate;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public Date getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
-	}
 }
