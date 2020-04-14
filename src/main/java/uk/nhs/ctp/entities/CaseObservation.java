8b84d77716e68a6802c3057367a28f5747e70ee7
Charlie Marcus <charlie.marcus1@nhs.net>
2020-04-14 10:49:55 +0100

    check for existing observation before adding new one on handover

--- src/main/java/uk/nhs/ctp/entities/CaseObservation.java
+++ src/main/java/uk/nhs/ctp/entities/CaseObservation.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -22,12 +23,14 @@ public class CaseObservation {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Exclude
   private Long id;
 
   @JsonIgnore
   @ManyToOne(optional = false)
   @JoinColumn(name = "case_id")
-  @ToString.Exclude()
+  @ToString.Exclude
+  @EqualsAndHashCode.Exclude
   private Cases caseEntity;
 
   @Column(name = "system")
@@ -59,9 +62,11 @@ public class CaseObservation {
 
   @CreationTimestamp
   @Column(name = "created")
+  @EqualsAndHashCode.Exclude
   private Date dateCreated;
 
   @UpdateTimestamp
   @Column(name = "updated")
+  @EqualsAndHashCode.Exclude
   private Date dateUpdated;
 }
