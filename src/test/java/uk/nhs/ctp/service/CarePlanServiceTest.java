f3a749c90343895c7f580d9823c481480c586437
Charlie Marcus <charlie.marcus1@nhs.net>
2020-05-12 13:06:50 +0100

    care plan service test

---
+++ src/test/java/uk/nhs/ctp/service/CarePlanServiceTest.java
@@ -0,0 +1,71 @@
+package uk.nhs.ctp.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.mockito.Mockito.when;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import java.util.List;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.dstu3.model.CarePlan;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.nhs.ctp.service.fhir.ReferenceService;
+import uk.nhs.ctp.service.fhir.StorageService;
+import uk.nhs.ctp.testhelper.MockingUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarePlanServiceTest {
+
+  private CarePlanService carePlanService;
+
+  @Mock
+  private StorageService storageService;
+
+  @Mock
+  private ReferenceService referenceService;
+
+  @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+  private IGenericClient mockClient;
+
+  @Before
+  public void setup() {
+    when(storageService.getClient()).thenReturn(mockClient);
+    carePlanService = new CarePlanService(
+        storageService,
+        referenceService
+    );
+  }
+
+  @Test
+  public void shouldFetchCarePlansByCaseId() {
+    CarePlan expected = new CarePlan()
+        .setContext(new Reference("123"))
+        .setDescription("desc");
+    Bundle returns = new Bundle()
+        .addEntry(new BundleEntryComponent()
+          .setResource(expected));
+    MockingUtils.mockSearch(mockClient, CarePlan.class, returns);
+
+    List<CarePlan> results = carePlanService.getByCaseId(3L);
+
+    assertThat(results, contains(expected));
+  }
+
+  @Test
+  public void shouldReturnEmptyListNoCarePlansFound() {
+    MockingUtils.mockSearch(mockClient, CarePlan.class, new Bundle());
+
+    List<CarePlan> results = carePlanService.getByCaseId(3L);
+
+    assertThat(results, empty());
+  }
+
+}
