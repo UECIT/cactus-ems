7ae0502eaf1718ac8e85a52455144ee3abb7c07f
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-05-15 14:25:23 +0100

    Updated the live EMS_API url for the production environment.

--- EMS-UI/src/environments/environment.prod.ts
+++ EMS-UI/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  EMS_API: '/api',
+  EMS_API: 'https://ems.cactus-staging.iucdspilot.uk',
   UECDI_API: 'http://emstestharness-ems-care-advice.eu-west-2.elasticbeanstalk.com',
   UECDI_VALIDATE_API: 'http://uecdi-tom-tkw.eu-west-2.elasticbeanstalk.com/tkw-client',
-
   TERM_API: 'https://ontoserver.dataproducts.nhs.uk/fhir',
 };
