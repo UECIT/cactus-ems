53b5564732d85f4cecea1663987c61fe9e0b72db
Charlie Marcus <charlie.marcus1@nhs.net>
2020-05-26 13:45:15 +0100

    add roles to tests

--- src/test/java/uk/nhs/ctp/controllers/UserControllerTest.java
+++ src/test/java/uk/nhs/ctp/controllers/UserControllerTest.java
@@ -3,12 +3,16 @@ package uk.nhs.ctp.controllers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.nhs.ctp.model.RegisterSupplierRequest;
 import uk.nhs.ctp.model.SupplierAccountDetails;
@@ -20,7 +24,11 @@ public class UserControllerTest {
   @Autowired
   private UserController userController;
 
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
   @Test
+  @WithMockUser(roles = "ADMIN")
   public void shouldResponseWithAccountDetails() {
     RegisterSupplierRequest request = new RegisterSupplierRequest();
     request.setSupplierId("testid");
@@ -29,11 +37,22 @@ public class UserControllerTest {
   }
 
   @Test
+  @WithMockUser(roles = "ADMIN")
   public void shouldNotResponseError() {
     RegisterSupplierRequest request = new RegisterSupplierRequest();
 
     ResponseEntity<SupplierAccountDetails> response = userController.signup(request);
 
     assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+  }
+
+  @Test
+  @WithMockUser
+  public void shouldFailRole() {
+    RegisterSupplierRequest request = new RegisterSupplierRequest();
+    request.setSupplierId("testid");
+
+    expectedException.expect(AccessDeniedException.class);
+    userController.signup(request);
   }
 }
