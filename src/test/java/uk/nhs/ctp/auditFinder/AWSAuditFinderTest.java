015b5d2688ed2b1dd065a7db485c30dee148093f
Charlie Marcus <charlie.marcus1@nhs.net>
2020-06-25 12:31:38 +0100

    add audit finder unit tests

--- src/test/java/uk/nhs/ctp/auditFinder/AWSAuditFinderTest.java
+++ src/test/java/uk/nhs/ctp/auditFinder/AWSAuditFinderTest.java
@@ -13,7 +13,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.nhs.ctp.testhelper.matchers.IsEqualJSON.equalToJSON;
@@ -57,7 +56,7 @@ public class AWSAuditFinderTest {
   private ElasticSearchClient elasticSearchClient;
 
   @Mock
-  private TokenAuthenticationService tokenAuthenticationService;
+  private TokenAuthenticationService authService;
 
   @Mock
   private ObjectMapper objectMapper;
@@ -70,9 +69,7 @@ public class AWSAuditFinderTest {
 
   @Test
   public void findAll_withNullClient_shouldFail() {
-    var authService = mock(TokenAuthenticationService.class);
-    when(authService.requireSupplierId()).thenReturn("test-supplier");
-    var auditFinder = new AWSAuditFinder(null, authService, mock(ObjectMapper.class));
+    var auditFinder = new AWSAuditFinder(null, authService, objectMapper);
 
     expectedException.expect(IllegalArgumentException.class);
     auditFinder.findAll(1L);
@@ -80,18 +77,14 @@ public class AWSAuditFinderTest {
 
   @Test
   public void findAll_withCaseIdAndSupplierId_buildsRequest() throws IOException {
-    when(tokenAuthenticationService.requireSupplierId())
-        .thenReturn("test-supplier");
-    when(elasticSearchClient.search(
-        argThat(is("test-supplier-audit")),
-        any(SearchSourceBuilder.class)))
+    when(authService.requireSupplierId()).thenReturn("test-supplier");
+    when(elasticSearchClient.search(eq("test-supplier-audit"), any(SearchSourceBuilder.class)))
         .thenReturn(Collections.emptyList());
 
     auditFinder.findAll(76L);
 
     var searchSourceCaptor = ArgumentCaptor.forClass(SearchSourceBuilder.class);
-    verify(elasticSearchClient)
-        .search(argThat(is("test-supplier-audit")), searchSourceCaptor.capture());
+    verify(elasticSearchClient).search(eq("test-supplier-audit"), searchSourceCaptor.capture());
 
     var searchSource = searchSourceCaptor.getValue();
 
@@ -124,11 +117,8 @@ public class AWSAuditFinderTest {
         .map(this::buildSearchHit)
         .collect(Collectors.toUnmodifiableList());
 
-    when(tokenAuthenticationService.requireSupplierId())
-        .thenReturn("test-supplier");
-    when(elasticSearchClient.search(
-        argThat(is("test-supplier-audit")),
-        any(SearchSourceBuilder.class)))
+    when(authService.requireSupplierId()).thenReturn("test-supplier");
+    when(elasticSearchClient.search(eq("test-supplier-audit"), any(SearchSourceBuilder.class)))
         .thenReturn(searchHits);
     when(objectMapper.readValue(auditSessionJson, AuditSession.class))
         .thenReturn(auditSession);
@@ -149,8 +139,7 @@ public class AWSAuditFinderTest {
   public void findAll_withFailedParsing_shouldFail() throws IOException {
     var auditSessionJson = "{ \"requestUrl\": \"/test-url\" }";
 
-    when(tokenAuthenticationService.requireSupplierId())
-        .thenReturn("test-supplier");
+    when(authService.requireSupplierId()).thenReturn("test-supplier");
     when(elasticSearchClient.search(
         argThat(is("test-supplier-audit")),
         any(SearchSourceBuilder.class)))
@@ -160,6 +149,73 @@ public class AWSAuditFinderTest {
 
     expectedException.expect(JsonParseException.class);
     auditFinder.findAll(76L);
+  }
+
+  @Test
+  public void findEncounters_nullClient_shouldFail() {
+    var auditFinder = new AWSAuditFinder(null, authService, objectMapper);
+
+    expectedException.expect(IllegalArgumentException.class);
+    auditFinder.findAllEncounters();
+  }
+
+  @Test
+  public void findEncounters_buildsRequest() throws Exception {
+    when(authService.requireSupplierId()).thenReturn("test-supplier");
+    when(elasticSearchClient.search(eq("test-supplier-audit"), any(SearchSourceBuilder.class)))
+        .thenReturn(Collections.emptyList());
+
+    auditFinder.findAllEncounters();
+
+    var searchSourceCaptor = ArgumentCaptor.forClass(SearchSourceBuilder.class);
+    verify(elasticSearchClient).search(eq("test-supplier-audit"), searchSourceCaptor.capture());
+
+    var searchSource = searchSourceCaptor.getValue();
+
+    assertThat(searchSource.sorts(), hasSize(1));
+    assertThat(searchSource.sorts().get(0),
+        hasToString(equalToJSON("{ @timestamp : {order : asc } }")));
+    assertThat(searchSource.query(), hasToString(equalToJSON(
+        "{ bool : { must : ["
+            + " { term : { additionalProperties.supplierId : { value : test-supplier } } },"
+            + " { exists : { field : additionalProperties.caseId } }"
+            + "] } }")));
+  }
+
+  @Test
+  public void findEncounter_returnsAudits() throws IOException {
+    var auditSessionJson = "{ \"requestUrl\": \"/test-url\" }";
+    var auditSession = AuditSession.builder().requestUrl("/test-url").build();
+    var auditSessionWithEntryJson = "{"
+        + " \"requestUrl\": \"/test-url-2\","
+        + " \"entries\": ["
+        + " { \"requestUrl\": \"/test-url-3\" }"
+        + "] }";
+    var auditSessionWithEntry = AuditSession.builder()
+        .requestUrl("/test-url-2")
+        .entry(AuditEntry.builder().requestUrl("/test-url-3").build())
+        .build();
+
+    var searchHits = Stream.of(auditSessionJson, auditSessionWithEntryJson)
+        .map(this::buildSearchHit)
+        .collect(Collectors.toUnmodifiableList());
+
+    when(authService.requireSupplierId()).thenReturn("test-supplier");
+    when(elasticSearchClient.search(eq("test-supplier-audit"), any(SearchSourceBuilder.class)))
+        .thenReturn(searchHits);
+    when(objectMapper.readValue(auditSessionJson, AuditSession.class))
+        .thenReturn(auditSession);
+    when(objectMapper.readValue(auditSessionWithEntryJson, AuditSession.class))
+        .thenReturn(auditSessionWithEntry);
+
+    var audits = auditFinder.findAllEncounters();
+
+    //noinspection unchecked
+    assertThat(audits, hasItems(
+        hasRequestUrl("/test-url"),
+        both(hasRequestUrl("/test-url-2"))
+            .and(hasProperty("entries",
+                hasItem(hasRequestUrl("/test-url-3"))))));
   }
 
   @Test
