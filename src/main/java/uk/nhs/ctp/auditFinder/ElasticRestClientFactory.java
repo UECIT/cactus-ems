2f3bf43c6e3f0e33dacad1716513cfbc0898a4e0
Charlie Marcus <charlie.marcus1@nhs.net>
2020-07-15 10:01:15 +0100

    refactor to use apache client for opendistro

---
+++ src/main/java/uk/nhs/ctp/auditFinder/ElasticRestClientFactory.java
@@ -0,0 +1,66 @@
+package uk.nhs.ctp.auditFinder;
+
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.google.common.base.Preconditions;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ElasticRestClientFactory {
+
+  private static final String ES_SERVICE_NAME = "es";
+  private static final Pattern AWS_ES_PATTERN =
+      Pattern.compile("https://[a-z0-9-]+\\.([a-z0-9-]+)\\.es\\.amazonaws\\.com");
+
+  public RestHighLevelClient highLevelClient(String endpoint) {
+    Preconditions.checkState(
+        StringUtils.isNotEmpty(endpoint),
+        "Expected non-empty endpoint for ElasticSearch client");
+
+    var baseClientBuilder = RestClient.builder(HttpHost.create(endpoint));
+
+    interceptor(endpoint)
+        .ifPresentOrElse(interceptor -> baseClientBuilder.setHttpClientConfigCallback(clientConfig ->
+            clientConfig.addInterceptorLast(interceptor)),
+            () -> log.info("Creating an ElasticSearchClient for a non-AWS endpoint"));
+
+    return new RestHighLevelClient(baseClientBuilder);
+  }
+
+  public CloseableHttpClient httpClient(String endpoint) {
+    HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+    interceptor(endpoint)
+        .map(httpClientBuilder::addInterceptorFirst);
+
+    return httpClientBuilder.build();
+  }
+
+  private Optional<AWSRequestSigningApacheInterceptor> interceptor(String endpoint) {
+    var awsEndpointMatcher = AWS_ES_PATTERN.matcher(endpoint);
+    if (awsEndpointMatcher.matches()) {
+      log.info("Creating an ElasticSearchClient for an AWS endpoint");
+
+      var signer = new AWS4Signer();
+      signer.setServiceName(ES_SERVICE_NAME);
+      signer.setRegionName(awsEndpointMatcher.group(1));
+
+      return Optional.of(new AWSRequestSigningApacheInterceptor(
+          ES_SERVICE_NAME,
+          signer,
+          new DefaultAWSCredentialsProviderChain()));
+    }
+    return Optional.empty();
+  }
+
+}
--- src/main/java/uk/nhs/ctp/auditFinder/ElasticSearchClient.java
+++ src/main/java/uk/nhs/ctp/auditFinder/ElasticSearchClient.java
@@ -1,73 +1,38 @@
 package uk.nhs.ctp.auditFinder;
 
-import com.amazonaws.auth.AWS4Signer;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.google.common.base.Preconditions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.regex.Pattern;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.security.PutRoleMappingRequest;
-import org.elasticsearch.client.security.PutRoleMappingResponse;
-import org.elasticsearch.client.security.PutRoleRequest;
-import org.elasticsearch.client.security.PutRoleResponse;
-import org.elasticsearch.client.security.RefreshPolicy;
-import org.elasticsearch.client.security.support.expressiondsl.fields.FieldRoleMapperExpression;
-import org.elasticsearch.client.security.user.privileges.IndicesPrivileges;
-import org.elasticsearch.client.security.user.privileges.Role;
 import org.elasticsearch.client.security.user.privileges.Role.IndexPrivilegeName;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import uk.nhs.ctp.auditFinder.role.PutRoleMappingRequest;
+import uk.nhs.ctp.auditFinder.role.PutRoleRequest;
+import uk.nhs.ctp.auditFinder.role.PutRoleRequest.IndexPermissions;
 
 @Component
 @Slf4j
 @Profile("!dev")
+@RequiredArgsConstructor
 public class ElasticSearchClient {
 
-  private static final String ES_SERVICE_NAME = "es";
-  private static final Pattern AWS_ES_PATTERN =
-      Pattern.compile("https://[a-z0-9-]+\\.([a-z0-9-]+)\\.es\\.amazonaws\\.com");
+  @Value("${es.audit}")
+  private String endpoint;
 
-  private final RestHighLevelClient baseClient;
-
-  public ElasticSearchClient(@Value("${es.audit}") String endpoint) {
-    Preconditions.checkState(
-        StringUtils.isNotEmpty(endpoint),
-        "Expected non-empty endpoint for ElasticSearch client");
-
-    var baseClientBuilder = RestClient.builder(HttpHost.create(endpoint));
-
-    var awsEndpointMatcher = AWS_ES_PATTERN.matcher(endpoint);
-    if (awsEndpointMatcher.matches()) {
-      log.info("Creating an ElasticSearchClient for an AWS endpoint");
-
-      var signer = new AWS4Signer();
-      signer.setServiceName(ES_SERVICE_NAME);
-      signer.setRegionName(awsEndpointMatcher.group(1));
-
-      var interceptor = new AWSRequestSigningApacheInterceptor(
-          ES_SERVICE_NAME,
-          signer,
-          new DefaultAWSCredentialsProviderChain());
-      baseClientBuilder.setHttpClientConfigCallback(clientConfig ->
-          clientConfig.addInterceptorLast(interceptor));
-    } else {
-      log.info("Creating an ElasticSearchClient for a non-AWS endpoint");
-    }
-
-    this.baseClient = new RestHighLevelClient(baseClientBuilder);
-  }
+  private final ElasticRestClientFactory clientFactory;
+  private final ObjectMapper objectMapper;
 
   public List<SearchHit> search(String index, SearchSourceBuilder source) throws IOException {
     var request = new SearchRequest()
@@ -77,43 +42,33 @@ public class ElasticSearchClient {
     log.info("Sending ElasticSearch request to index " + index + ":");
     log.info(request.toString());
 
-    var response = baseClient.search(request, RequestOptions.DEFAULT);
+    var response = clientFactory.highLevelClient(endpoint)
+        .search(request, RequestOptions.DEFAULT);
     return Arrays.asList(response.getHits().getHits());
   }
 
   public void mapRole(String supplierId, String username) throws IOException {
+    CloseableHttpClient client = clientFactory.httpClient(endpoint);
     String roleName = supplierId + "_role";
-    Role supplierRole = Role.builder()
-        .name(roleName)
-        .indicesPrivileges(IndicesPrivileges.builder()
-            .privileges(IndexPrivilegeName.READ)
-            .indices(supplierId + "-*")
+
+    PutRoleRequest roleRequest = PutRoleRequest.builder()
+        .indexPermission(IndexPermissions.builder()
+            .allowedAction(IndexPrivilegeName.READ)
+            .indexPattern(supplierId + "-*")
             .build())
         .build();
-    PutRoleRequest putRoleRequest = new PutRoleRequest(supplierRole, RefreshPolicy.NONE);
-    PutRoleResponse putRoleResponse = baseClient.security()
-        .putRole(putRoleRequest, RequestOptions.DEFAULT);
 
-    if (!putRoleResponse.isCreated()) {
-      log.error("Could not create role for supplier");
-      return;
-    }
+    HttpHost host = new HttpHost(endpoint);
+    HttpPut putRoleRequest = new HttpPut("_opendistro/_security/api/roles/" + roleName);
+    putRoleRequest.setEntity(new ByteArrayEntity(objectMapper.writeValueAsBytes(roleRequest)));
+    client.execute(host, putRoleRequest);
 
-    PutRoleMappingRequest mappingRequest = new PutRoleMappingRequest(
-        roleName + "_mapping",
-        true,
-        Collections.singletonList(roleName),
-        Collections.emptyList(),
-        FieldRoleMapperExpression.ofUsername(username),
-        null,
-        RefreshPolicy.NONE
-    );
+    PutRoleMappingRequest roleMappingRequest = PutRoleMappingRequest.builder()
+        .user(username)
+        .build();
 
-    PutRoleMappingResponse putRoleMappingResponse = baseClient.security()
-        .putRoleMapping(mappingRequest, RequestOptions.DEFAULT);
-
-    if (!putRoleMappingResponse.isCreated()) {
-      log.error("Could not create role mapping for supplier");
-    }
+    HttpPut putRoleMappingRequest = new HttpPut(" _opendistro/_security/api/rolesmapping/" + roleName);
+    putRoleMappingRequest.setEntity(new ByteArrayEntity(objectMapper.writeValueAsBytes(roleMappingRequest)));
+    client.execute(host, putRoleMappingRequest);
   }
 }
