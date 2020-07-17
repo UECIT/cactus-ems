48e19811ce7c2a29a57f98945933bed1e8ac5e1c
cmarcus-nhs <60224748+cmarcus-nhs@users.noreply.github.com>
2020-07-17 09:39:48 +0100

    swap service category to use new valueset (#77)

--- src/main/java/uk/nhs/ctp/enums/ServiceCategory.java
+++ src/main/java/uk/nhs/ctp/enums/ServiceCategory.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ServiceCategory implements Concept {
-  SPECIALIST_MEDICAL("27", "Specialist Medical");
 
-  private final String system = "http://hl7.org/fhir/service-category";
+  ART_MUSIC_DRAMA("R0540", "Art, Music & Drama Student");
+
+  private final String system = "https://fhir.hl7.org.uk/STU3/CodeSystem/CareConnect-SDSJobRoleName-1";
   private final String value;
   private final String display;
 }
