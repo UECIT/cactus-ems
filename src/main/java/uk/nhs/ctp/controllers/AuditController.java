7d2a7e6e1ebfced96e31027f65c60abdc244a2d7
Mike Houston <60653100+mhouston-nhs@users.noreply.github.com>
2020-07-21 14:01:45 +0100

    Refactor audit validation to allow downloading evidence zip (#85)
    * Refactor audit validation to allow downloading evidence zip
    
    * Add test for download endpoint

--- src/main/java/uk/nhs/ctp/controllers/AuditController.java
+++ src/main/java/uk/nhs/ctp/controllers/AuditController.java
@@ -4,12 +4,16 @@ import static org.thymeleaf.util.StringUtils.isEmpty;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +29,7 @@ import uk.nhs.ctp.caseSearch.CaseSearchRequest;
 import uk.nhs.ctp.caseSearch.CaseSearchResultDTO;
 import uk.nhs.ctp.caseSearch.CaseSearchService;
 import uk.nhs.ctp.tkwvalidation.ValidationService;
+import uk.nhs.ctp.tkwvalidation.model.AuditMetadata;
 
 @CrossOrigin
 @RestController
@@ -33,64 +38,84 @@ import uk.nhs.ctp.tkwvalidation.ValidationService;
 @Slf4j
 public class AuditController {
 
-	private final ObjectMapper mapper;
-	private final AuditFinder auditFinder;
-	private final CaseSearchService caseSearchService;
-	private final ValidationService validationService;
+  private final ObjectMapper mapper;
+  private final AuditFinder auditFinder;
+  private final CaseSearchService caseSearchService;
+  private final ValidationService validationService;
 
-	/**
-	 * Retrieve all audit sessions for a given case ID.
-	 * Because this search requires the case id property existing, this will return sessions during a triage, namely: $evaluate.
-	 * @param id Case ID to search on
-	 * @return list of Audit Sessions
-	 */
-	@GetMapping(path = "/{id}")
-	public String getAudit(@PathVariable Long id) throws IOException {
-		return mapper.writeValueAsString(auditFinder.findAllEmsEncountersByCaseId(id.toString()));
-	}
+  /**
+   * Retrieve all audit sessions for a given case ID. Because this search requires the case id
+   * property existing, this will return sessions during a triage, namely: $evaluate.
+   *
+   * @param id Case ID to search on
+   * @return list of Audit Sessions
+   */
+  @GetMapping(path = "/{id}")
+  public String getAudit(@PathVariable Long id) throws IOException {
+    return mapper.writeValueAsString(auditFinder.findAllEmsEncountersByCaseId(id.toString()));
+  }
 
-	@GetMapping(path = "/encounters")
-	public List<AuditSession> getAuditEncounters() {
-		return auditFinder.findAllEncounters();
-	}
+  @GetMapping(path = "/encounters")
+  public List<AuditSession> getAuditEncounters() {
+    return auditFinder.findAllEncounters();
+  }
 
-	@GetMapping(path = "/servicesearches")
-	public List<AuditSession> getAuditServiceSearch() {
-		return auditFinder.findAllServiceSearches();
-	}
+  @GetMapping(path = "/servicesearches")
+  public List<AuditSession> getAuditServiceSearch() {
+    return auditFinder.findAllServiceSearches();
+  }
 
-	@PostMapping(path = "/validate")
-	public void validate(@RequestBody AuditValidationRequest request) throws IOException {
-		// TODO CDSCT-400: unify different queries
+  private List<AuditSession> findAudits(AuditValidationRequest request) {
+    // TODO CDSCT-400: unify different queries
+    if (!isEmpty(request.getCaseId())) {
+      // client must be asking for evaluate requests
+      return auditFinder.findAllEncountersByCaseId(request.getCaseId());
+    }
 
-		if (!isEmpty(request.getCaseId())) {
-			// client must be asking for evaluate requests
-			var audits = auditFinder.findAllEncountersByCaseId(request.getCaseId());
-			validationService.validateAudits(
-					audits,
-					request.getType(),
-					request.getInstanceBaseUrl());
-			return;
-		}
+    if (!isEmpty(request.getSearchAuditId())) {
+      var audit = auditFinder.findByAuditId(request.getSearchAuditId())
+          .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+      return List.of(audit);
+    }
 
-		if (!isEmpty(request.getSearchAuditId())) {
-			var audit = auditFinder.findByAuditId(request.getSearchAuditId())
-					.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
-			validationService.validateAudits(
-					Collections.singletonList(audit),
-					request.getType(),
-					request.getInstanceBaseUrl());
-			return;
-		}
+    throw new HttpClientErrorException(
+        HttpStatus.BAD_REQUEST,
+        "Must specify either caseId or searchAuditId");
+  }
 
-		throw new HttpClientErrorException(
-				HttpStatus.BAD_REQUEST,
-				"Must specify either caseId or searchAuditId");
-	}
-	
-	@PostMapping
-	public Page<CaseSearchResultDTO> getAudit(@RequestBody CaseSearchRequest request) {
-		return caseSearchService.search(request);
-	}
+  @PostMapping(path = "/validate")
+  public void validate(@RequestBody AuditValidationRequest request) throws IOException {
+    var audits = findAudits(request);
+    validationService.validateAudits(
+        audits,
+        request.getType(),
+        request.getInstanceBaseUrl());
+  }
+
+
+  @PostMapping(path = "/download")
+  public ResponseEntity<byte[]> download(@RequestBody AuditValidationRequest request)
+      throws IOException {
+
+    var audits = findAudits(request);
+
+    var zipData = validationService.getZipData(audits, request.getType());
+    AuditMetadata metadata = validationService
+        .getAuditMetadata(audits, request.getType(), request.getInstanceBaseUrl());
+
+    BodyBuilder response = ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType("application/zip"))
+        .header(HttpHeaders.CONTENT_DISPOSITION,
+            // TODO this filename could be based on the interaction ID
+            "attachment; filename=\"" + UUID.randomUUID().toString() + ".zip\"")
+        .headers(metadata.toHeaders());
+
+    return response.body(zipData);
+  }
+
+  @PostMapping
+  public Page<CaseSearchResultDTO> getAudit(@RequestBody CaseSearchRequest request) {
+    return caseSearchService.search(request);
+  }
 
 }
--- src/main/java/uk/nhs/ctp/tkwvalidation/ValidationService.java
+++ src/main/java/uk/nhs/ctp/tkwvalidation/ValidationService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.nhs.ctp.audit.model.AuditSession;
 import uk.nhs.ctp.auditFinder.model.OperationType;
+import uk.nhs.ctp.tkwvalidation.model.AuditMetadata;
 import uk.nhs.ctp.tkwvalidation.rules.AuditValidationRule;
 
 @Service
@@ -23,16 +24,27 @@ public class ValidationService {
       OperationType operationType,
       String selectedServiceEndpoint)
       throws IOException {
+
+    byte[] zipData = getZipData(audits, operationType);
+    AuditMetadata zipMetadata = getAuditMetadata(audits, operationType, selectedServiceEndpoint);
+
+    auditDispatcher.dispatchToTkw(zipData, zipMetadata);
+  }
+
+  public AuditMetadata getAuditMetadata(List<AuditSession> audits, OperationType operationType,
+      String selectedServiceEndpoint) {
+    return auditMetadataCollector.collect(audits, operationType, selectedServiceEndpoint);
+  }
+
+  public byte[] getZipData(List<AuditSession> audits, OperationType operationType)
+      throws IOException {
     validationRules.stream()
         .filter(rule -> rule.getSupportedType() == operationType)
         .findFirst()
         .ifPresent(rule -> rule.ensure(audits));
 
     var messageAudits = auditSelector.selectAudits(audits, operationType);
-    var zipData = auditZipBuilder.zipMessageAudits(messageAudits);
-    var zipMetadata = auditMetadataCollector.collect(audits, operationType, selectedServiceEndpoint);
-
-    auditDispatcher.dispatchToTkw(zipData, zipMetadata);
+    return auditZipBuilder.zipMessageAudits(messageAudits);
   }
 
   public String getValidationUrl() {
