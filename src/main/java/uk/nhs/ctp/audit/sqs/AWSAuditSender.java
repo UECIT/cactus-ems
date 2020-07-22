cb6a508034cf19d5eba70e1cde4e7c295c31164b
Mike Houston <60653100+mhouston-nhs@users.noreply.github.com>
2020-07-22 10:40:14 +0100

    When logging audit sending error, truncate the audit to 1KB if larger. (#87)
    * When logging audit sending error, truncate the audit to 1KB if larger.
    * Don't log stack trace for anticipated SQS errors

--- src/main/java/uk/nhs/ctp/audit/sqs/AWSAuditSender.java
+++ src/main/java/uk/nhs/ctp/audit/sqs/AWSAuditSender.java
@@ -23,53 +23,63 @@ import uk.nhs.ctp.audit.model.AuditSession;
 @Profile("!dev")
 public class AWSAuditSender implements AuditSender {
 
-    private static final String SENDER = "sender";
-    private static final String SUPPLIER = "supplierId";
+  private static final String SENDER = "sender";
+  private static final String SUPPLIER = "supplierId";
 
-    @Value("${sqs.audit.queue}")
-    private String loggingQueue;
+  @Value("${sqs.audit.queue}")
+  private String loggingQueue;
 
-    @Value("${service.name}")
-    private String serviceName;
+  @Value("${service.name}")
+  private String serviceName;
 
-    private final ObjectMapper mapper;
-    private final AmazonSQS sqsClient;
-    private final TokenAuthenticationService authenticationService;
+  private final ObjectMapper mapper;
+  private final AmazonSQS sqsClient;
+  private final TokenAuthenticationService authenticationService;
 
-    @Override
-    public void sendAudit(AuditSession session) {
-        Preconditions.checkArgument(isNotEmpty(loggingQueue), "SQS Queue url must be provided");
+  @Override
+  public void sendAudit(AuditSession session) {
+    Preconditions.checkArgument(isNotEmpty(loggingQueue), "SQS Queue url must be provided");
 
-        var supplierId = authenticationService.getCurrentSupplierId();
+    var supplierId = authenticationService.getCurrentSupplierId();
 
-        if (supplierId.isEmpty()) {
-            log.warn("No supplier id, not sending audit");
-            return;
-        }
-
-        try {
-            SendMessageRequest request = new SendMessageRequest()
-                .withMessageGroupId(supplierId.get())
-                .withMessageDeduplicationId(UUID.randomUUID().toString())
-                .addMessageAttributesEntry(SENDER, stringAttribute(serviceName))
-                .addMessageAttributesEntry(SUPPLIER, stringAttribute(supplierId.get()))
-                .withQueueUrl(loggingQueue)
-                .withMessageBody(mapper.writeValueAsString(session));
-            sqsClient.sendMessage(request);
-        } catch (AmazonSQSException e) {
-            if (e.getStatusCode() == 413) {
-                log.warn("Audit request exceeded max size SQS can handle", e);
-                //TODO: CDSCT-338 - Should we be auditing/paging audit search audits or excluding things
-            }
-            log.error("an error occurred sending audit session {} to SQS", session, e);
-        } catch (Exception e) {
-            log.error("an error occurred sending audit session {} to SQS", session, e);
-        }
+    if (supplierId.isEmpty()) {
+      log.warn("No supplier id, not sending audit");
+      return;
     }
 
-    private MessageAttributeValue stringAttribute(String value) {
-        return new MessageAttributeValue()
-                .withDataType("String")
-                .withStringValue(value);
+    try {
+      SendMessageRequest request = new SendMessageRequest()
+          .withMessageGroupId(supplierId.get())
+          .withMessageDeduplicationId(UUID.randomUUID().toString())
+          .addMessageAttributesEntry(SENDER, stringAttribute(serviceName))
+          .addMessageAttributesEntry(SUPPLIER, stringAttribute(supplierId.get()))
+          .withQueueUrl(loggingQueue)
+          .withMessageBody(mapper.writeValueAsString(session));
+      sqsClient.sendMessage(request);
+    } catch (AmazonSQSException e) {
+      if (e.getStatusCode() == 413) {
+        log.warn("Audit request exceeded max size SQS can handle", e);
+        //TODO: CDSCT-338 - Should we be auditing/paging audit search audits or excluding things
+      }
+      log.error("an error occurred sending audit session {} to SQS: {}", format(session), e.getErrorMessage());
+    } catch (Exception e) {
+      log.error("an error occurred sending audit session {} to SQS", format(session), e);
     }
+  }
+
+  private String format(AuditSession session) {
+    String sessionString = session.toString();
+    if (sessionString.length() < 1 << 10) {
+      return sessionString;
+    }
+
+    return String.format("%s... (truncated from %d characters)",
+        sessionString.substring(0, 1 << 10), sessionString.length());
+  }
+
+  private MessageAttributeValue stringAttribute(String value) {
+    return new MessageAttributeValue()
+        .withDataType("String")
+        .withStringValue(value);
+  }
 }
