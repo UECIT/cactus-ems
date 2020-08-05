d099112a3226a55f97095627f17aee198fff5f40
Mike Houston <60653100+mhouston-nhs@users.noreply.github.com>
2020-08-05 10:24:33 +0100

    Render Observation value and QuestionnaireResponse answer codes with display text (#113)

--- src/main/java/uk/nhs/ctp/transform/ConditionDTOTransformer.java
+++ src/main/java/uk/nhs/ctp/transform/ConditionDTOTransformer.java
@@ -4,22 +4,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.apache.commons.collections4.Transformer;
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.Condition;
 import org.hl7.fhir.dstu3.model.Condition.ConditionEvidenceComponent;
 import org.hl7.fhir.dstu3.model.Observation;
 import org.hl7.fhir.dstu3.model.QuestionnaireResponse;
+import org.hl7.fhir.dstu3.model.Type;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.springframework.stereotype.Component;
-import uk.nhs.ctp.service.fhir.GenericResourceLocator;
-import uk.nhs.ctp.service.fhir.ReferenceService;
 import uk.nhs.ctp.service.dto.ConditionDTO;
+import uk.nhs.ctp.service.fhir.GenericResourceLocator;
 
 @Component
 @AllArgsConstructor
 public class ConditionDTOTransformer implements Transformer<Condition, ConditionDTO> {
 
-  private GenericResourceLocator resourceLocator;
-  private ReferenceService referenceService;
+  private final GenericResourceLocator resourceLocator;
 
   @Override
   public ConditionDTO transform(Condition condition) {
@@ -52,16 +53,51 @@ public class ConditionDTOTransformer implements Transformer<Condition, Condition
   }
 
   private String observationString(Observation observation) {
-    return "Observation - " + observation.getCode().getCodingFirstRep()
-        .getDisplay() + " = " + observation.getValue();
+    return "Observation - " + typeString(observation.getCode())
+        + " = " + typeString(observation.getValue());
   }
 
   private String questionnaireResponseString(QuestionnaireResponse questionnaireResponse) {
     return questionnaireResponse.getItem().stream()
         .map(item -> "Question - " + item.getText() + " = " + item.getAnswer().stream()
-            .map(answer -> answer.getValue().toString())
+            .map(answer -> typeString(answer.getValue()))
             .collect(Collectors.joining("/")))
         .collect(Collectors.joining(", "));
+  }
+
+  private String typeString(Type value) {
+    if (value == null) {
+      return "null";
+    } else if (value instanceof Coding) {
+      return codingString((Coding) value);
+    } else if (value instanceof CodeableConcept) {
+      return codeableConceptString((CodeableConcept) value);
+    } else {
+      return String.format("%s[%s]", value.getClass().getSimpleName(), value.toString());
+    }
+  }
+
+  private String codeableConceptString(CodeableConcept codeableConcept) {
+    if (codeableConcept.hasText()) {
+      return codeableConcept.getText();
+    } else if (codeableConcept.hasCoding()) {
+      return codeableConcept.getCoding().stream()
+          .map(this::codingString)
+          .collect(Collectors.joining(", "));
+    }
+    return codeableConcept.toString();
+  }
+
+  private String codingString(Coding coding) {
+    if (coding.hasDisplay()) {
+      return coding.getDisplay();
+    }
+
+    StringBuilder sb = new StringBuilder(coding.getCode());
+    if (coding.hasSystem()) {
+      sb.append(" (").append(coding.getSystem()).append(")");
+    }
+    return sb.toString();
   }
 
 }
