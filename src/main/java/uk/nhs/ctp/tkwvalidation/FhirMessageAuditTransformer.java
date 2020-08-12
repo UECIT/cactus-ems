50fe9cc36404a9c3cc8238f1e606770da3823f7b
Michael Houston <michael.houston3@nhs.net>
2020-08-12 11:18:01 +0100

    Update audit zip to use correct host if available

--- src/main/java/uk/nhs/ctp/tkwvalidation/FhirMessageAuditTransformer.java
+++ src/main/java/uk/nhs/ctp/tkwvalidation/FhirMessageAuditTransformer.java
@@ -3,9 +3,11 @@ package uk.nhs.ctp.tkwvalidation;
 import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 import static org.springframework.http.MediaType.parseMediaType;
 
+import com.google.common.base.Strings;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -38,33 +40,43 @@ public class FhirMessageAuditTransformer {
         ? session.getResponseBody()
         : null;
 
+    String fullUrl = buildSessionURL(session);
     return FhirMessageAudit.builder()
-        .filePath(mergePaths(basePath, session.getRequestUrl()))
-        .fullUrl(fullUrl(session.getRequestHeaders(), session.getRequestUrl()))
+        .filePath(mergePaths(basePath, fullUrl))
+        .fullUrl(fullUrl)
         .requestBody(requestBody)
         .responseBody(responseBody)
         .moment(session.getCreatedDate())
         .build();
   }
 
-  private String fullUrl(String requestHeaders, String requestUrl) {
+  private String buildSessionURL(AuditSession session) {
     final var HOST_HEADER = HttpHeaders.HOST.toLowerCase();
-    var allHeaders = auditParser.getHeadersFrom(defaultIfEmpty(requestHeaders, ""));
-    if (!allHeaders.containsKey(HOST_HEADER)) {
-      return "";
-    }
+    var allHeaders =
+        auditParser.getHeadersFrom(defaultIfEmpty(session.getRequestHeaders(), ""));
 
-    StringBuilder sb = new StringBuilder();
-    sb.append(singleHeader(allHeaders, "x-forwarded-proto", "http"));
-    sb.append("://").append(singleHeader(allHeaders, "host", "unknown-host"));
-    sb.append(requestUrl);
-
-    return sb.toString();
+    URI requestUrl = URI.create(session.getRequestUrl());
+    return String.format("%s://%s%s",
+        defaultString(singleHeader(allHeaders, "x-forwarded-proto"),
+            requestUrl.getScheme(), "http"),
+        defaultString(singleHeader(allHeaders, "host"),
+            requestUrl.getHost(), "unknown-host"),
+        requestUrl.getPath());
   }
 
-  private String singleHeader(Map<String, Collection<String>> headers, String name,
-      String defaultValue) {
-    return headers.getOrDefault(name, List.of(defaultValue)).iterator().next();
+  private String singleHeader(Map<String, Collection<String>> headers, String name) {
+    return headers.getOrDefault(name, Collections.emptyList())
+        .stream().findFirst()
+        .orElse(null);
+  }
+
+  private String defaultString(String... strings) {
+    for (String s : strings) {
+      if (!Strings.isNullOrEmpty(s)) {
+        return s;
+      }
+    }
+    return null;
   }
 
   public FhirMessageAudit from(AuditEntry entry, String basePath, boolean includeRequestBody) {
@@ -86,9 +98,13 @@ public class FhirMessageAuditTransformer {
 
   private String mergePaths(String base, String path) {
     var uri = URI.create(path);
-    var uriPath = uri.getHost() + uri.getPath();
+    StringBuilder sb = new StringBuilder(base);
+    if (uri.isAbsolute()) {
+      sb.append("/").append(uri.getHost());
+    }
+    sb.append(uri.getPath());
 
-    return String.format("%s/%s", base, uriPath);
+    return sb.toString();
   }
 
   private boolean isFhirContent(String headersString) {
